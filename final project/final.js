0037141f6e909393f7e48bb7238f1e0079d2402e
ArthurLiao0816 <adamliao0816@yahoo.com>
2019-06-13 11:03:10 +0800

    final-home.html

--- final project/final.js
+++ final project/final.js
@@ -1,7 +1,7 @@
 function calculate() {
-    var fstn = document.getElementById('fstn')
-    var operator = document.getElementById('operator')
-    var scdn = document.getElementById('scdn')
-    var result = document.getElementById('result')
+    var fstn = document.getElementById("fstn")
+    var operator = document.getElementById("operator")
+    var scdn = document.getElementById("scdn")
+    var result = document.getElementById("result")
     result.innerText = eval(fstn.value + operator.value + scdn.value)
   }
